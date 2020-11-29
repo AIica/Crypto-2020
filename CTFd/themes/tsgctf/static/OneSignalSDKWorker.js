e2252c9d044aa0173e61a12a86eb69b3dd74e87f
Alica Lancraft <alica.lancraft@gmail.com>
2020-11-29 16:00:22 +0300

    Fixed logout

--- CTFd/themes/tsgctf/static/OneSignalSDKWorker.js
+++ CTFd/themes/tsgctf/static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1606592505946', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1606654731312', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')

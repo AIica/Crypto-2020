b42aee5a6e02701c1f8c383f14984084f19d9f23
Alica Lancraft <alica.lancraft@gmail.com>
2020-12-05 13:37:02 +0300

    Updated theme

--- CTFd/themes/tsgctf/static/OneSignalSDKWorker.js
+++ CTFd/themes/tsgctf/static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1607163892324', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1607164316367', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')

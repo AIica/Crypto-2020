28b9dedabf47d608e7e6b165ad30fc2df3b3299e
Alica Lancraft <alica.lancraft@gmail.com>
2020-12-05 13:27:46 +0300

    Updated theme

--- CTFd/themes/tsgctf/static/OneSignalSDKWorker.js
+++ CTFd/themes/tsgctf/static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1606654731312', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1607163892324', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
--- dispatcher.py
+++ dispatcher.py
@@ -12,4 +12,4 @@ app.wsgi_app = DispatcherMiddleware(NotFound(), {
 })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
